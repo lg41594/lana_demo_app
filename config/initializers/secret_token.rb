5f32c663c5ce5f0a2e60a6869d8f2d15888f63e6
Lana <gerichenator@gmail.com>
2012-01-13 09:39:25 +0100

    prva verzija bez koda

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LanaDemoApp::Application.config.secret_token = '8c5ca428f42f5b2877d2dccd6ae335865d5cff7202299f4e756fc1b353a5460de1c63e438fc31fbf0845a40f91e070b881621da5dd0b3201d9e96eefc8698056'
